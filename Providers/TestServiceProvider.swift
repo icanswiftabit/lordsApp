a6e96d945f90a16501cd069dc0ddf098923b9772
Błażej Wdowikowski <blazej@wdowikowski.pl>
2017-08-29 19:24:43 +0200

    add project

---
+++ Providers/TestServiceProvider.swift
@@ -0,0 +1,13 @@
+//
+//  TestServiceProvider.swift
+//  LordsApp
+//
+//  Copyright © 2017 Netguru Sp. z o.o. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+final class TestServiceProvider {
+    
+}
